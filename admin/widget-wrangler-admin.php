47cae55dafe4d1941c3783ed958853c24c6c6b3c
daggerhart <daggerhart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-27 01:32:49 +0000

    Fixing issue saving widgets to custom post types.  Cleaned up loading of assets so that they only load on WW screens.
    git-svn-id: https://plugins.svn.wordpress.org/widget-wrangler/trunk@955431 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/widget-wrangler-admin.php
+++ admin/widget-wrangler-admin.php
@@ -96,9 +96,8 @@ class Widget_Wrangler_Admin {
     if (!empty($this->ww->settings['post_types'])){
       foreach($this->ww->settings['post_types'] as $enabled_post_type){
         add_meta_box('ww_admin_meta_box', __('<img src="'.WW_PLUGIN_URL.'/admin/images/lasso-small-black.png" />Widget Wrangler'), array( $this, '_sortable_widgets_meta_box'), $enabled_post_type, 'normal', 'high');
-        
         // Add some CSS to the admin header on the widget wrangler pages, and edit pages
-        if ($this->ww->admin->_is_editing_enabled_post_type()){
+        if ($this->_is_editing_enabled_post_type()){
           $this->init_sortable_widgets();
         }
         
@@ -111,8 +110,8 @@ class Widget_Wrangler_Admin {
    
   // add js and css for sortable widgets
   function init_sortable_widgets(){
-      add_action( 'admin_enqueue_scripts', array( $this, '_sortable_widgets_js' ) );
-      add_action( 'admin_head', array( $this, '_admin_css' ) );
+    add_action( 'admin_enqueue_scripts', array( $this, '_sortable_widgets_js' ) );
+    add_action( 'admin_head', array( $this, '_admin_css' ) );
   }
   
   //
@@ -504,7 +503,7 @@ class Widget_Wrangler_Admin {
     
     // Ensure this is an enabled post_type and user can edit it
     $settings = $this->ww->settings;  
-    if (in_array($_POST['post_type'], $settings['post_types']) && !current_user_can('edit_page', $post_id)){
+    if (!in_array($_POST['post_type'], $settings['post_types']) || !current_user_can('edit_post', $post_id)){
       return $post_id;
     }
     
@@ -594,7 +593,7 @@ class Widget_Wrangler_Admin {
   //
   function _sortable_widgets_js(){
     wp_enqueue_script('ww-sortable-widgets',
-                    WW_PLUGIN_URL.'/admin/js/sortable-widgets.js',
+                    WW_PLUGIN_URL.'admin/js/sortable-widgets.js',
                     array('jquery-ui-core', 'jquery-ui-sortable'),
                     false,
                     true);
@@ -607,7 +606,7 @@ class Widget_Wrangler_Admin {
   // 
   function _editing_widget_js(){
     wp_enqueue_script('ww-editing-widget',
-                    WW_PLUGIN_URL.'/admin/js/editing-widget.js',
+                    WW_PLUGIN_URL.'admin/js/editing-widget.js',
                     array('jquery'),
                     false,
                     true);
